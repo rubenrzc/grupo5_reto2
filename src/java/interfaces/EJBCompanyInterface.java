f098c89296c1ac5787410b7c23808e2e8bdef76c
stone <ruben.rzc8@gmail.com>
2019-12-19 12:39:09 +0100

    Company para merge

---
+++ src/java/interfaces/EJBCompanyInterface.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import entitiesJPA.Company;
+import entitiesJPA.User;
+import exceptions.CreateException;
+import exceptions.DeleteException;
+import exceptions.GetCollectionException;
+import exceptions.SelectException;
+import exceptions.UpdateException;
+import java.util.Collection;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author stone
+ */
+@Local
+public interface EJBCompanyInterface {
+
+    public void createCompany(Company company) throws CreateException;
+
+    public void updateCompany(Company company) throws UpdateException;
+
+    public void deleteCompany(Company company) throws DeleteException;
+
+    public Company getCompanyProfile(int id) throws SelectException;
+
+    public Collection<Company> getCompanyList() throws GetCollectionException;
+
+}
